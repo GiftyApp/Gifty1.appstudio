3642a05d25344bb1ba24fccb3b9841b6c932a821
Ryan Ledger <ryanledger@creighton.edu>
2019-11-24 13:15:46 -0600

    Initial commit

---
+++ forms/amazonPage/amazonPage.js
@@ -0,0 +1,17 @@
+
+
+<script type="text/javascript">
+amzn_assoc_placement = "adunit0";
+amzn_assoc_tracking_id = "454405-20";
+amzn_assoc_ad_mode = "search";
+amzn_assoc_ad_type = "smart";
+amzn_assoc_marketplace = "amazon";
+amzn_assoc_region = "US";
+amzn_assoc_default_search_phrase = "Apple watch";
+amzn_assoc_default_category = "All";
+amzn_assoc_linkid = "66e682e03dafe5dd4b13577e7086bda9";
+amzn_assoc_search_bar = "true";
+amzn_assoc_search_bar_position = "top";
+amzn_assoc_title = "Shop Related Products";
+</script>
+<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US"></script>
